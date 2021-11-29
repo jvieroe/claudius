55a3a4ce6286d4fda277c38370a9e209eb281782
jvieroe <43220957+jvieroe@users.noreply.github.com>
2021-11-29 14:16:33 +0100

    read_baselayer

---
+++ R/read_baselayer.R
@@ -0,0 +1,26 @@
+#' Get Denmark baselayer shapefile
+#'
+#' lorem ipsum
+#' @param as_sf A logical value. Return data as an object of class `sf`? Defauls to `TRUE`. If `FALSE`, data is imported as a GEOJSON of class `geomultipolygon`
+#' @return data lorem ipsum
+#' @author Jeppe Vierø
+#' @import sf dplyr
+#' @export
+
+read_baselayer <- function(as_sf = TRUE) {
+
+  baselayer <- claudius::baselayer
+
+  if (as_sf == TRUE) {
+
+    base_layer <- geojsonsf::geojson_sf(baselayer)
+
+  } else if (as_sf == FALSE) {
+
+    base_layer <- baselayer
+
+  }
+
+  return(base_layer)
+
+}
