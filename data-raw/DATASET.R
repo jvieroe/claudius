a93f5092bed16c01c3a2078f61560430f4bd3059
jvieroe <43220957+jvieroe@users.noreply.github.com>
2021-11-29 13:02:02 +0100

    baselayer data etc

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -1,3 +1,10 @@
 ## code to prepare `DATASET` dataset goes here
+baselayer <- sf::read_sf("https://api.dataforsyningen.dk/regioner?format=geojson")
+baselayer <- sf::st_transform(baselayer,
+                              crs = 4326)
+baselayer <- sf::st_union(baselayer)
+baselayer <- geojsonio::geojson_json(baselayer)
 
-usethis::use_data(DATASET, overwrite = TRUE)
+usethis::use_data(baselayer, overwrite = TRUE, compress = "xz")
+
+tools::add_datalist(pkgpath = "C:/Users/jevi/OneDrive - Epinion/Documents/GitHub/claudius")
